b07f7adae5b19c9dab545fd03d4becf929c921a2
yu-prgram <12ym.nkmr.11ym@gmail.com>
2020-01-10 17:52:09 +0900

    1月10日-2

---
+++ collection/src/jp/co/internous/action/List.java
@@ -0,0 +1,28 @@
+package jp.co.internous.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ListSample {
+
+	public static void main(String[] args) {
+//		Listをインスタンスする
+		List <String> users = new ArrayList<>();
+
+//		add()メソッドで要素(値)を追加する
+		users.add("Taro");
+		users.add("Ichiko");
+		users.add("Jiro");
+
+//		get()メソッドで要素を取得する。配列と同様でindex(添え字)で取得する。
+	String user1 = users.get(0);
+	String user2 = users.get(1);
+	String user3 = users.get(2);
+
+//		consoleに表示
+	System.out.println(user1);
+	System.out.println(user2);
+	System.out.println(user3);
+
+	}
+}
